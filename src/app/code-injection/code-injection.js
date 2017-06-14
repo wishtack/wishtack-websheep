cddc991bffb4b2ef1ffd2fa725ec76ba2f486196
Younes JAAIDI <yjaaidi@gmail.com>
2017-06-14 23:23:33 +0200

    v0.17.0:     Added code injection solution.

--- src/app/code-injection/code-injection.js
+++ src/app/code-injection/code-injection.js
@@ -11,11 +11,21 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const router = express.Router();
 
+const { config } = require('../config');
+
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => res.render('code-injection/code-injection', {
+    shouldShowSolution: config.shouldShowSolution(),
     title: 'Code Injection'
 }));
+
+if (config.shouldShowSolution()) {
+    router.get('/solution', (req, res) => res.render('code-injection/code-injection-solution', {
+        title: 'Code Injection - Solution'
+    }));
+}
+
 
 router.post('/formulas', (req, res) => res.send({
     formula: req.body.formula,
